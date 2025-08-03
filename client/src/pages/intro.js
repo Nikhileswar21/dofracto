14022a93199ba151feb91bf38fe8a7d9bf8c2f18
Balaji V <balajivadivel05@gmail.com>
2025-08-03 18:03:29 +0530

    bug fixes login

--- client/src/pages/intro.js
+++ client/src/pages/intro.js
@@ -5,9 +5,9 @@ import logo from "../assets/logo2.png"; // Adjust path to your logo file
 export default function Intro() {
   return (
     <div className="intro-container">
-      <img src={logo} alt="DoFracto Logo" className="intro-logo" />
+      <img src={logo} alt="Dofracto Logo" className="intro-logo" />
       <p className="intro-text">Start your Journey with</p>
-      <h1 className="intro-title">DoFracto</h1>
+      <h1 className="intro-title">Dofracto</h1>
     </div>
   );
 }
